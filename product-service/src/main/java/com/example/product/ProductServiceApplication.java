1011d3d17dae422cf6ad54e907d4ae9200a56529
colaptrinhvien <112139221+colaptrinhvien@users.noreply.github.com>
2023-08-02 19:42:37 +0700

    Add files via upload

--- product-service/src/main/java/com/example/product/ProductServiceApplication.java
+++ product-service/src/main/java/com/example/product/ProductServiceApplication.java
@@ -1,15 +1,19 @@
 package com.example.product;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@OpenAPIDefinition
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
-
 }
